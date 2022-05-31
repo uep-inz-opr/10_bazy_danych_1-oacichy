909cdce79c82a95cffade73b7be1ccb701e342b8
oacichy <68201@student.ue.poznan.pl>
2022-06-01 01:20:21 +0200

    dodanie inputa

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
-df2=pd.read_csv('polaczenia_duze.csv')
-result = df2['duration'].sum()
-print(result)
+
+if __name__ == '__main__':
+    plik = input()
+    df2=pd.read_csv(plik)
+    result = df2['duration'].sum()
+    print(result)
