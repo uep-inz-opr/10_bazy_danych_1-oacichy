221168bdc1d72f529d18d08b0b469c1588f616a9
oacichy <68201@student.ue.poznan.pl>
2022-06-01 01:38:10 +0200

    bez pandas

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import pandas as pd
+import csv
 
 if __name__ == '__main__':
     plik = input()
-    df2=pd.read_csv(plik)
-    result = df2['duration'].sum()
-    print(result)
+    data = csv.reader(open(plik))
+    next(data)
+    sum = 0
+    for row in data:
+        value = int(row[3])
+        sum += value
+
+    print(sum)
+
